f61710c72d35afe338fd6e613302e6a370c83cbe
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-02-08 13:19:43 +0100

    Add files via upload

---
+++ eau_ok.py
@@ -0,0 +1,6 @@
+def retourne_liste(input_chaine , input_liste) :
+    return f"{input_chaine} {input_liste}"
+
+input_chaine = "J'ai terminé l'Epreuve de eau et c'etait"
+input_liste = ["Bien"]
+print(retourne_liste(input_chaine , input_liste))
