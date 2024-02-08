05c34f0910e797cdf32330cd70c5cee1134e2c38
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-02-08 13:20:28 +0100

    Add files via upload

---
+++ Tri_par_selection.py
@@ -0,0 +1,23 @@
+import sys
+
+def my_bulle_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+if len(sys.argv) < 2: 
+    print("Erreur : Veuillez fournir au moin un nombre ")
+    sys.exit(1)
+
+
+try:
+    numbers = [int (x) for x in sys.argv[1:]]
+except ValueError:
+    print("Erreur : Les arguments doivent etre des nombres")
+    sys.exit(1)
+    
+result = my_bulle_sort(numbers)
+print("Liste triée :" , result)
